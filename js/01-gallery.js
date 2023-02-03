229a5c45100a045b9f48ffe5753fc4691e190d75
Inna-Kovalishina <114028931+Inna-Kovalishina@users.noreply.github.com>
2023-02-03 21:08:58 +0200

    Додає скрипти у відповідності до завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery')
+
+const makeImageItemMarkup = ({preview, original, description}) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`
+}
+
+const makeImageGalleryMarkup = galleryItems.map(makeImageItemMarkup).join('');
+
+galleryEl.innerHTML = makeImageGalleryMarkup;
+
+galleryEl.addEventListener('click', onOpenModal);
+
+function onOpenModal(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" alt="${event.target.alt}" width="800" height="600">`,
+      {
+        onShow: () => { document.addEventListener('keydown', closeModalWithEsc); },
+        onClose: () => { document.removeEventListener('keydown', closeModalWithEsc); },
+      })
+
+  instance.show();
+
+  function closeModalWithEsc(event) {
+    if (event.key === 'Escape') {
+      instance.close();
+    }
+  }
+}
+
+
+
+// console.log(makeImageGalleryMarkup);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery')
+
+const makeImageItemMarkup = ({preview, original, description}) => {
+    return `<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a>`
+}
+
+const makeImageGalleryMarkup = galleryItems.map(makeImageItemMarkup).join('');
+
+galleryEl.innerHTML = makeImageGalleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a',
+  { captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+
+
+console.log(galleryEl);
